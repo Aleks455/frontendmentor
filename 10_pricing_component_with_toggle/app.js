e16969a7438aa2f1ff6c47e2c9dd618229c16ab3
Aleksandra555 <alekspetrovic.it@gmail.com>
2022-08-30 15:54:27 +0200

    Frontend mentor challenge 10

---
+++ 10_pricing_component_with_toggle/app.js
@@ -0,0 +1,9 @@
+function toggle()
+{
+    var toggle = document.getElementById('toggle');
+    var show = document.getElementById('card-container');
+
+    show.classList.toggle('show-monthly');
+   
+    console.log(show);
+}
